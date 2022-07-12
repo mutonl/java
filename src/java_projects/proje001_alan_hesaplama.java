269a70ec67061db5c188e214397a12dd87a17cbd
Muhammed Topaç <mtopac2018@gmail.com>
2022-07-12 03:54:15 +0300

    Create proje001_alan_hesaplama.java

---
+++ src/java_projects/proje001_alan_hesaplama.java
@@ -0,0 +1,11 @@
+package java_projects;
+
+public class proje001_alan_hesaplama {
+
+	public static void main(String[] args) {
+		
+		int yuzey=6;
+		int alan= yuzey*yuzey*yuzey;
+		System.out.println("alan:"+alan);
+	}
+}
